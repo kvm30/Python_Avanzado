db21a91c9a1803ade738bf28f53de2322e4e1bda
Carol Herrera <karolherrera3006@gmail.com>
2026-02-21 11:19:26 -0500

    mi primer commit

---
+++ paradigma_imperativo.py
@@ -0,0 +1,15 @@
+#PARADIGMA IMPERATIVO
+
+#PARADIGMA IMPERATIVO
+#Aquí damos instrucciones paso a paso
+
+placas = []     #creamos listas vacias
+placas.append("ABC123")    #Agregamos placa
+placas.append("XYZ999")   #Agregamos otra placa
+
+print("Vehiculos actuales:", placas)   #mostramos listas
+
+placas.remove("ABC123")  #Quitamos un vehiculo
+
+print("Después de la salida:", placas) #mostramos resultado
+##Le decimos a la computadora paso a paso que hacer
---
+++ paradigma_procedural.py
@@ -0,0 +1,26 @@
+##Usamos funciones para organizar el código
+
+
+#PARADIGMA PROCEDURAL
+
+
+def registrar_entrada(lista, placa):
+    #Agrega un vehículo a la lista
+    lista.append(placa)
+
+def registrar_salida(lista, placa):
+    #Elimina vehículo si existe
+    if placa in lista:
+        lista.remove(placa)
+
+
+vehículos = []
+
+registrar_entrada(vehículos, "AAA111")
+registrar_entrada(vehículos, "BBB222")
+
+print("Vehículos:", vehículos)
+
+registrar_salida(vehículos, "AAA111")
+
+print("Después salida", vehículos)
